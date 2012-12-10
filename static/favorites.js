5ff28387275504fafa02a718e368f20aef47fe7e
Derek Tia <dtia05@gmail.com>
2012-12-09 21:59:53 -0800

    add map

--- static/favorites.js
+++ static/favorites.js
@@ -1,3 +1,11 @@
+function initialize() {
+	map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), {
+		credentials:"AkL7bdBJmEpRncJpZ9MmXwEUIvnkId7Yo9IJrDhNFGrkR--08hfritan5JH_nzRw",
+		center: new Microsoft.Maps.Location(37.7577,-122.4376),
+		mapTypeId: Microsoft.Maps.MapTypeId.road,
+		zoom: 4});
+}
+
 function listFavorites() {
 	$.get('/', function(data) {
 		$('#page').html(data);
